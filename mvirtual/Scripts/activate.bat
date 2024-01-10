708b2a9763698434305c1e7dd1607ba91c204841
Rag-x <juanj.bravomax@gmail.com>
2024-01-09 23:14:49 -0500

    comit

--- mvirtual/Scripts/activate.bat
+++ mvirtual/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User\Desktop\ProyectoData_Center\mvirtual
+set VIRTUAL_ENV=C:\Users\User\Desktop\Rag-x.github.io\mvirtual
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,6 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(mvirtual) 
 
 :END
 if defined _OLD_CODEPAGE (
